8356e15c9c4a979262bf3ee14a88ef253fa6c0d2
Freddiefady <dodofady49@gmail.com>
2025-11-21 02:16:04 +0200

    feat(dev-tools) formatting by tools and setup in composer for tools.

--- database/migrations/2025_11_20_215240_create_orders_table.php
+++ database/migrations/2025_11_20_215240_create_orders_table.php
@@ -1,7 +1,9 @@
 <?php
 
-    use App\Enums\StatusOrder;
-    use Illuminate\Database\Migrations\Migration;
+declare(strict_types=1);
+
+use App\Enums\StatusOrder;
+use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
@@ -9,7 +11,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table): void {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('order_number')->unique();
